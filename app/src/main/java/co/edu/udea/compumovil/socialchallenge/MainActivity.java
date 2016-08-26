3b2561653feea7ac939ed2964c3b006ca36db306
Steven <jsteven.yepes@gmail.com>
2016-08-26 13:53:33 -0500

    Tabs titles was modificated and

--- app/src/main/java/co/edu/udea/compumovil/socialchallenge/MainActivity.java
+++ app/src/main/java/co/edu/udea/compumovil/socialchallenge/MainActivity.java
@@ -150,11 +150,11 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Retos";
                 case 1:
-                    return "SECTION 2";
+                    return "Perfil";
                 case 2:
-                    return "SECTION 3";
+                    return "Actividad";
             }
             return null;
         }
