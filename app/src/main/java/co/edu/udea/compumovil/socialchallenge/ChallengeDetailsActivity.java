d16d3b98eea7b492a24ad6beb4dbfa547601c6af
Steven <jsteven.yepes@gmail.com>
2016-11-12 21:27:33 -0500

    exp bar

--- app/src/main/java/co/edu/udea/compumovil/socialchallenge/ChallengeDetailsActivity.java
+++ app/src/main/java/co/edu/udea/compumovil/socialchallenge/ChallengeDetailsActivity.java
@@ -4,15 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import co.edu.udea.compumovil.socialchallenge.bl.ExperienceController;
 import co.edu.udea.compumovil.socialchallenge.entities.Challenge;
 import co.edu.udea.compumovil.socialchallenge.entities.Task;
 
@@ -20,6 +26,9 @@ public class ChallengeDetailsActivity extends AppCompatActivity {
 
     private RecyclerView listChallenges;
     private DatabaseReference mDatabase;
+    private DatabaseReference userDatabase;
+    private DatabaseReference userExp;
+    private static final String TAG = "ChallengeDetailsTAG";
     FirebaseAuth auth;
 
     @Override
@@ -36,6 +45,9 @@ public class ChallengeDetailsActivity extends AppCompatActivity {
             auth = FirebaseAuth.getInstance();
             mDatabase = FirebaseDatabase.getInstance().getReference().child("challenges")
                     .child(auth.getCurrentUser().getUid()).child(challengeID).child("tasks");
+            userDatabase = FirebaseDatabase.getInstance().getReference().child("users")
+                    .child(auth.getCurrentUser().getUid());
+            userExp = userDatabase.child("exp");
             mDatabase.keepSynced(true);
         }
     }
@@ -53,10 +65,57 @@ public class ChallengeDetailsActivity extends AppCompatActivity {
                 ) {
 
                     @Override
-                    protected void populateViewHolder(MessageViewHolder viewHolder, Task model, int position) {
-                        viewHolder.mText.setText(model.getName());
-                        viewHolder.mFrom.setText(model.getTimeBegin());
-                        viewHolder.mTo.setText(model.getTimeEnd());
+                    protected void populateViewHolder(MessageViewHolder viewHolder, Task model, final int position) {
+
+
+                        if(!model.isFinished()) {
+                            viewHolder.mText.setText(model.getName());
+                            viewHolder.mFrom.setText(model.getTimeBegin());
+                            viewHolder.mTo.setText(model.getTimeEnd());
+                            viewHolder.mCheck.setChecked(false);
+
+                            viewHolder.mCheck.setVisibility(View.VISIBLE);
+                            viewHolder.mText.setVisibility(View.VISIBLE);
+                            viewHolder.mFrom.setVisibility(View.VISIBLE);
+                            viewHolder.mTo.setVisibility(View.VISIBLE);
+                            viewHolder.tvFrom.setVisibility(View.VISIBLE);
+                            viewHolder.tvTo.setVisibility(View.VISIBLE);
+                        } else {
+
+                            viewHolder.mCheck.setVisibility(View.GONE);
+                            viewHolder.mText.setVisibility(View.GONE);
+                            viewHolder.mFrom.setVisibility(View.GONE);
+                            viewHolder.mTo.setVisibility(View.GONE);
+                            viewHolder.tvFrom.setVisibility(View.GONE);
+                            viewHolder.tvTo.setVisibility(View.GONE);
+                        }
+                        viewHolder.mCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                                if(b) {
+                                    Log.d(TAG, "Task finish:" + getRef(position));
+                                    getRef(position).child("finished").setValue(true);
+
+                                    userExp.addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                            int exp = dataSnapshot.getValue(Integer.class);
+                                            int newExp = exp + 100;
+                                            userExp.setValue(newExp);
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError databaseError) {
+
+                                        }
+                                    });
+
+
+                                }else {
+                                    getRef(position).child("finished").setValue(false);
+                                }
+                            }
+                        });
                     }
                 };
 
@@ -70,6 +129,8 @@ public class ChallengeDetailsActivity extends AppCompatActivity {
         CheckBox mCheck;
         TextView mFrom;
         TextView mTo;
+        TextView tvTo;
+        TextView tvFrom;
 
         public MessageViewHolder(View itemView) {
             super(itemView);
@@ -77,8 +138,11 @@ public class ChallengeDetailsActivity extends AppCompatActivity {
             mCheck = (CheckBox) itemView.findViewById(R.id.check1);
             mFrom = (TextView) itemView.findViewById(R.id.text_from);
             mTo = (TextView) itemView.findViewById(R.id.text_to);
+            tvFrom = (TextView) itemView.findViewById(R.id.tvFrom);
+            tvTo = (TextView) itemView.findViewById(R.id.tv_to);
         }
 
 
     }
-}
+
+ }
